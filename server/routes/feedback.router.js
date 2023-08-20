3c3f64566d7033ed2b638bff9791c255d5ee3156
Gabe Glasco <gabeglasco@gmail.com>
2023-08-20 16:52:35 -0500

    add post with errors

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -4,10 +4,22 @@ const pool = require('../modules/pool');
 
 // GET all feedback info that has been submitted
 
-
-
-
-
 // POST feedback to DB
+router.post('/', (req, res) => {
+    let newFeedback = req.body;
+    console.log('Adding new feedback:', newFeedback);
+
+    let queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+                     VALUES ($1, $2, $3, $4);`;
+    pool.query(queryText, [newFeedback.feeling, newFeedback.understanding, newFeedback.support, newFeedback.comments])
+        .then(result => {
+            res.sendStatus(201);
+        })
+        .catch(error => {
+            console.log('Error adding new feedback:', error);
+            res.sendStatus(500);
+        });
+});
+
 
 module.exports = router;
--- src/components/SummaryView/SummaryView.jsx
+++ src/components/SummaryView/SummaryView.jsx
@@ -2,22 +2,47 @@ import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import { useState } from 'react';
 import React from 'react';
 import {useSelector, useDispatch} from 'react-redux';
+import axios from 'axios';
 
-
+// View all ratings and comments that have been entered
 function SummaryView() {
 
-const feedback = useSelector(store => store.feedbackInfo)
+// access feedbackInfo reducer (array of feedback)
+const feedbackInfo = useSelector(store => store.feedbackInfo)
+
+// Send feedbackInfo to DB
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    let newFeedback = {
+        feeling: feedbackInfo[0],
+        understanding: feedbackInfo[1],
+        support: feedbackInfo[2],
+        comments: feedbackInfo[3]
+    }
+
+   axios.post('/feedback', newFeedback);
+   console.log('data successfully submitted:', newFeedback)
+    .then(response => {
+
+    }).catch(error => {
+        console.error('Error submitting new feedback, /feedback:', error)
+    })
+} 
 
     return (
         <>
         <h1>Review Your Feedback</h1>
-            <h3>Feelings: {feedback[0]}</h3>
-            <h3>Understanding: {feedback[1]}</h3>
-            <h3>Support: {feedback[2]}</h3>
-            <h3>Comments: {feedback[3]}</h3>
+            <h3>Feelings: {feedbackInfo[0]}</h3>
+            <h3>Understanding: {feedbackInfo[1]}</h3>
+            <h3>Support: {feedbackInfo[2]}</h3>
+            <h3>Comments: {feedbackInfo[3]}</h3>
 
             <Link to="/submission">
-                <button id="submit">SUBMIT</button>
+                <button id="submit"
+                        onClick={handleSubmit}>
+                        SUBMIT
+                </button>
             </Link>
         </>
     )
